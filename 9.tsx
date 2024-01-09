c434e7073a6c7287ca3d89ed31b3487b01d6c1e0
大城亮 <hiltoc.katsu@gmail.com>
2024-01-09 21:38:35 +0900

    ユーザーアイコンの表示

---
+++ 9.tsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const MyComponent = () => (
+  <div>
+    <FontAwesomeIcon icon={faUser} /> {/* 例：ユーザーアイコン */}
+  </div>
+);
+/*ユーザーアイコンを表示でいるようにする */
